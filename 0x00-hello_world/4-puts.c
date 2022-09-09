6063cfab636eab6f653804b80375b026a81495a5
Ätahine <eatahine@gmail.com>
2022-09-09 21:00:01 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
   * * Description: using the main function
   * * this program prints "Programming is like building a multilingual puzzle
   * * Return: 0
-  * */
-int main(void) /* code */
+  */
+int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
