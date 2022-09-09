26ccbf921da00f401b4b72f9b55f99f79f42e9be
Ätahine <eatahine@gmail.com>
2022-09-09 19:46:49 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 */
 int main(void) /* code */
 {
-	
-puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+	
 }
