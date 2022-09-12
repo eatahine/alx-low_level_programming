ed52d79d806f7eb850edd86e409d2f12781a0e39
Ätahine <eatahine@gmail.com>
2022-09-12 11:20:11 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (n = '0'; n <= '9'; n++) /*print first two digit combo*/
 	{
-		for (n2 = '0'; n2 <= '9'; n2++)
+		for (n2 = n + '1'; n2 <= '9'; n2++)
 		{
-			for (n3 = n + 1; n3 <= '9'; n3++) /*print second of pair*/
+			for (n3 = n2 + 1; n3 <= '9'; n3++) /*print second of pair*/
 			{
-				for (n4 = n2 + 1; n4 <= '9'; n4++)
+				for (n4 = n3 + 1; n4 <= '9'; n4++)
 				{
 				putchar(n);
 				putchar(n2);
