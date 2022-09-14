75baf71eef176bcf4cdd364fba2b12a047f542ce
Ätahine <eatahine@gmail.com>
2022-09-14 03:14:50 +0100

    I am federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,11 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-		_putchar(h);
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
 		_putchar(':');
-		_putchar(m);
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 		_putchar('\n');
 		}
 	}
