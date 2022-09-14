502cf4f57c5e3aa5e3d4260d1594e4fb67209534
Ätahine <eatahine@gmail.com>
2022-09-14 05:59:33 +0100

    104-fibonacci

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,40 @@
 #include "main.h"
+
 /**
- * *print_times_table - Prints the n times table
- * *@n: number of times, less than 15 greater than or equal to zero
+ * * print_times_table - Prints the times table of the input,
+ * *                     starting with 0.
+ * * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	int m, f, a;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
-	{	for (m = 0; m <= n; m++)
-		{	for (f = 0; f <= n; f++)
-			{	a = f * m;
-				if (f == 0)
+	{
+		for (num = 0; num <= n; num++)
+		{
+		_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+				if (prod <= 99)
+				_putchar(' ');
+				if (prod <= 9)
+				_putchar(' ');
+				if (prod >= 100)
 				{
-				_putchar(a + '0');
+				_putchar((prod / 100) + '0');
+				_putchar(((prod / 10)) % 10 + '0');
 				}
-				if (a < 10 && f != 0)
+				else if (prod <= 99 && prod >= 10)
 				{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(a + '0');
+				_putchar((prod / 10) + '0');
 				}
-				else if (a >= 10 && a < 100)
-				{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((a / 10) + '0');
-				_putchar((a % 10) + '0');
-				}
-				else if (a >= 100)
-				{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((a / 100) + '0');
-				_putchar(((a / 10) % 10) + '0');
-				_putchar((a % 10) + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
-		_putchar('\n');
-		}
+			_putchar('\n');
+		}	
 	}
-}
+}	
