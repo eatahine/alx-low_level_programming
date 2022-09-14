4b582948161356a93b8f5379e1b8d5e0239dc1be
Ätahine <eatahine@gmail.com>
2022-09-14 03:30:32 +0100

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * *times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+	int n, f, a;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (f = 0; f < 10; f++)
+		{
+		a = f * n;
+		if (f == 0)
+		{
+		_putchar(a + '0');
+		}
+		if (a < 10 && f != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(a + '0');
+		} else if (a >= 10)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 7-print_last_digit -function that prints the last digit of a number
 8-24_hours.c -function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+9-times_table - function that prints the 9 times table, starting with 0
