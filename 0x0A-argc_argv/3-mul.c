ece2e79f0392beea7ecbc06346ecf99f68e93174
Ätahine <eatahine@gmail.com>
2022-10-01 01:43:42 +0100

    Argc, Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  *  * main - program that prints its name, followed by a new line
  *   * @argc: argument that counts argument input
